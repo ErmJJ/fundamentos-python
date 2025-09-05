b21297c3b84e8d0bbd8c83a7f264b4de7cb8f820
ErmJJ <jjuli714cr@outlook.com>
2025-09-04 21:43:07 -0600

    Fundamentos Parte 1

--- fundamentos-i.py
+++ fundamentos-i.py
@@ -1,6 +1,16 @@
-nombre = input('Dime tú nombre: ')
-edad = int(input('Dime tú edad: '))
+continuar = 'si'
+while continuar == 'si' or continuar == 's':
+    nombre = input('\nDime tú nombre: ')
+    edad = int(input('Dime tú edad: '))
+    
+    if edad > 18:
+        print('Eres mayor de edad\n')
+        print (f"Hola {nombre}, tienes: {edad}")
+    
+    elif edad == 18:
+        print('Tienes 18 años, apenas mayor de edad')    
+    
+    else:
+        print('No cumples con la mayoría de edad')
 
-print ("Hola " + nombre + ", " + "tienes:", edad)
-# y,
-print (f"Hola {nombre}, tienes: {edad}")
+    continuar = input('\n¿Quieres continuar? (si/no): \nResp/ ')
